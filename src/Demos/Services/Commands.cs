4d13303c0a642e6ef0d9f69f5e3df1a95ea088d6
brunomlopes <brunomlopes@gmail.com>
2010-04-05 02:18:03 +0100

    Simple version of the python command loader

--- src/Demos/Services/Commands.cs
+++ src/Demos/Services/Commands.cs
@@ -1,7 +1,11 @@
 using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using Demos.Commands;
+using IronPython.Hosting;
+using IronPython.Runtime;
+using Microsoft.Scripting.Hosting;
 
 namespace Demos.Services
 {
@@ -15,7 +19,9 @@ namespace Demos.Services
                 .Where(cls => typeof (ICommand).IsAssignableFrom(cls))
                 .Where(cls => !cls.IsInterface && !cls.IsAbstract);
 
-            AvailableCommands = commandClasses.Select(cls => Activator.CreateInstance(cls) as ICommand);
+            AvailableCommands = commandClasses
+                .Select(cls => Activator.CreateInstance(cls) as ICommand)
+                .Concat(GetPythonCommands());
         }
 
         public string ExecuteFromName(string name, string parameters)
@@ -24,7 +30,50 @@ namespace Demos.Services
 
             if(command == null) return String.Empty;
 
-            return command.Execute(parameters);
+            try
+            {
+                return command.Execute(parameters);
+            }
+            catch (Exception e)
+            {
+                return string.Format("Error executing command '{0}' : '{1}'", command.Name, e.Message);
+            }
+        }
+
+        private IEnumerable<ICommand> GetPythonCommands()
+        {
+            var engine = Python.CreateEngine();
+            engine.Runtime.LoadAssembly(this.GetType().Assembly);
+
+            var commands = new List<ICommand>();
+
+            var pythonFiles =
+                Directory
+                    .GetFiles("Commands")
+                    .Where(f => f.ToLowerInvariant().EndsWith("ipy"));
+
+            foreach (var pythonFile in pythonFiles)
+            {
+                try
+                {
+                    ScriptSource script = engine.CreateScriptSourceFromFile(pythonFile);
+                    CompiledCode code = script.Compile();
+                    ScriptScope scope = engine.CreateScope();
+                    
+                    code.Execute(scope);
+
+                    var pythonCommands = scope.GetItems()
+                        .Select(kvp => kvp.Value)
+                        .OfType<ICommand>();
+
+                    commands.AddRange(pythonCommands);
+                }
+                catch (Exception e)
+                {
+                    System.Diagnostics.Debug.Write(string.Format("Error with file {1}: {0}", e, pythonFile));
+                }
+            }
+            return commands;
         }
     }
 }
